a788dc279169f659f60b141e9922bfafc401e881
Jack Yeh <jckyeh@gmail.com>
2020-02-08 09:22:46 +0000

    initial commit

---
+++ empty-example/sketch.js
@@ -0,0 +1,10 @@
+// View here: file:///Users/jackyeh/repositories/tutorials/p5/empty-example/index.html
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(50, 50, 80, 80);
+}
