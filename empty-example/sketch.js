47666e9782f72391e8ba52b3e5ca42563ec7addf
Jack Yeh <jckyeh@gmail.com>
2020-02-17 09:18:36 +0000

    conditional, w noFill()

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   
   createCanvas(640, 480);
   // print("Hello");
-  background(0, 0, 0);
+  // background(0, 0, 0);
 }
 
 
@@ -18,14 +18,25 @@ function draw() {
   // background(r, g, 0);
   
   // draw ellipse
-  noStroke();
-  fill(255, 255, 255, 50);
-  ellipse(mouseX, mouseY, 20, 20);
+  // noStroke();
+  // fill(255, 255, 255, 50);
+  // ellipse(mouseX, mouseY, 20, 20);
 
   // random ellipses appear
   // noStroke()
   // fill(random(0, 255), random(0, 255), random(0, 255), 100)
   // ellipse(random(0, width), random(0, height), 20, 20)
+
+  // ellipse changes color and shape conditional
+  background(255, 0, 0)
+  noStroke()
+  noFill()
+
+  if (mouseX >= width/2) {
+    fill(255, 255, 200);
+  }
+
+  ellipse(width/2, height/2, 150, 150);
   
   // if (mouseIsPressed) {
   //   fill(0);
@@ -35,9 +46,8 @@ function draw() {
   // ellipse(mouseX, mouseY, 80, 80);
 }
     
-function mousePressed() {
-  background(0, 0, 0);
-
-  fill(0, 0, 0);
-  ellipse(mouseX, mouseY, 20, 20);
-}
+// function mousePressed() {
+//   background(0, 0, 0);
+//   fill(0, 0, 0);
+//   ellipse(mouseX, mouseY, 20, 20);
+// }
