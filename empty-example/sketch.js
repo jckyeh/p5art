24d548f67f299c0a94d000552cf4393dc4dd7870
Jack Yeh <jckyeh@gmail.com>
2020-02-17 09:54:58 +0000

    Bounce ball around

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,5 +1,12 @@
 // View here: file:///Users/jackyeh/repositories/tutorials/p5/empty-example/index.html
 
+let circle = {
+  x: 100,
+  y: 480/2,
+  diameter: 50
+};
+let increment = 6;
+
 function setup() {
   // put setup code here
   
@@ -7,7 +14,6 @@ function setup() {
   // print("Hello");
   // background(0, 0, 0);
 }
-
 
 function draw() {
   // put drawing code here
@@ -28,15 +34,13 @@ function draw() {
   // ellipse(random(0, width), random(0, height), 20, 20)
 
   // ellipse changes color and shape conditional
-  background(255, 0, 0)
-  noStroke()
-  noFill()
-
-  if (mouseX >= width/2) {
-    fill(255, 255, 200);
-  }
-
-  ellipse(width/2, height/2, 150, 150);
+  // background(255, 0, 0)
+  // noStroke()
+  // noFill()
+  // if (mouseX >= width/2) {
+  //   fill(255, 255, 200);
+  // }
+  // ellipse(width/2, height/2, 150, 150);
   
   // if (mouseIsPressed) {
   //   fill(0);
@@ -44,6 +48,16 @@ function draw() {
   //   fill(255);
   // }
   // ellipse(mouseX, mouseY, 80, 80);
+
+  // ball bounce back
+  background(0);
+  ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+  if (circle.x >= (width - circle.diameter/2) 
+    || circle.x <= circle.diameter/2) {
+    increment = increment*-1;
+  }
+  circle.x = circle.x + increment;
+  console.log(circle.x);
 }
     
 // function mousePressed() {
