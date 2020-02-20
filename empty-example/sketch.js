d103ae2b7c68e4110b5782cfedb548c8f739ad7c
Jack Yeh <jckyeh@gmail.com>
2020-02-20 10:39:06 +0000

    Bouncing ball in box

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -6,6 +6,7 @@ let circle = {
   diameter: 50
 };
 let increment = 6;
+let incrementy = 6;
 
 function setup() {
   // put setup code here
@@ -49,26 +50,31 @@ function draw() {
   // }
   // ellipse(mouseX, mouseY, 80, 80);
 
-  // // ball bounce back
-  // background(0);
-  // ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
-  // if (circle.x >= (width - circle.diameter/2) 
-  //   || circle.x <= circle.diameter/2) {
-  //   increment = increment*-1;
-  // }
-  // circle.x = circle.x + increment;
-  // console.log(circle.x);
-
-  // Draw grid of ellipses
+  // ball bounce back
   background(0);
-  strokeWeight(4);
-  stroke(255);
-  for (let x=0; x<=width; x+=circle.diameter) {
-    for (let y=0; y<=height; y+=circle.diameter) {
-      fill(random(255), 0, random(255));
-      ellipse(x, y, circle.diameter/2, circle.diameter/2);
-    }
+  ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+  if (circle.x >= (width - circle.diameter/2) 
+    || circle.x <= circle.diameter/2) {
+    increment = increment*-1;
   }
+  if (circle.y >= (height - circle.diameter/2) 
+    || circle.y <= circle.diameter/2) {
+    incrementy = incrementy*-1;
+  }
+  circle.x = circle.x + increment;
+  circle.y = circle.y + incrementy;
+  console.log(circle.x);
+
+  // // Draw grid of ellipses
+  // background(0);
+  // strokeWeight(4);
+  // stroke(255);
+  // for (let x=0; x<=width; x+=circle.diameter) {
+  //   for (let y=0; y<=height; y+=circle.diameter) {
+  //     fill(random(255), 0, random(255));
+  //     ellipse(x, y, circle.diameter/2, circle.diameter/2);
+  //   }
+  // }
 }
     
 // function mousePressed() {
